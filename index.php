aff4aa10d2d4e7f41c405925576eb6ffa6f9e3e5
TwanAsselbergs <twanasselbergs@outlook.com>
2024-05-29 15:05:31 +0200

    front-end

--- index.php
+++ index.php
@@ -1,19 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/stylesheet.css">
-    <title>CBRtheory</title>
-</head>
-<body>
-    <div class="hoofdpagina">
-    <h1>Welkom bij deze CBR theory les</h1>
-    <h3>klik op de startknop om te beginnen</h3>
-    </div>
-    <div id="button_loc">
-        <button class="start_button">Start</button>
-    </div>
 
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="css/stylesheet.css" />
+  <link rel="shortcut icon" href="https://www.cbr.nl/logo.jpg" type="image/x-icon" />
+  <title>CBRTheorie</title>
+</head>
+
+<body>
+  <header>
+    <nav>
+      <div class="nav-bar">
+        <div class="nav-title">
+          <h1>Welkom bij CBRTheorie!</h1>
+        </div>
+        <div class="nav-login">
+          <button class="login-button">
+            <div style="display: flex; align-items: center">
+              <svg width="36" height="36" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <defs>
+                  <circle id="a" cx="18" cy="18" r="18" />
+                </defs>
+                <g fill="none" fill-rule="evenodd">
+                  <use fill="#0588F0" xlink:href="#a" />
+                  <g stroke="#FFF" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                    <path d="M9.596 26.506c0-4.673 3.839-6.577 8.574-6.577 4.735 0 8.574 1.904 8.574 6.577H9.596zM22.127 12.366a4.28 4.28 0 0 0-4.287-4.275 4.28 4.28 0 0 0-4.287 4.275 4.28 4.28 0 0 0 4.287 4.275 4.28 4.28 0 0 0 4.287-4.275z" />
+                  </g>
+                </g>
+              </svg>
+              <span>Mijn CBR (Inloggen)</span>
+            </div>
+          </button>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <main>
+    <div class="container">
+      <a href="vraag.php">
+        <button class="start-button">Start</button>
+      </a>
+    </div>
+  </main>
+  <footer></footer>
 </body>
+
 </html>
--- vraag.php
+++ vraag.php
@@ -1,40 +1,44 @@
 <?php
-include ('database/db_connect.php');
+include('database/db_connect.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/vraag.css">
-    <title>Document</title>
+    <link rel="shortcut icon" href="https://www.cbr.nl/logo.jpg" type="image/x-icon" />
+    <title>CBRTheorie</title>
 </head>
+
 <body>
-    <h1>Vraag:"<?=$id ?>"</h1>
+    <h1><?= $id ?></h1>
 
     <div class="item">
         <div class="vraagtitel">
             <h2>Vraag 1</h2>
         </div>
-    <div class="image">
-    <img src="<?=$image ?>" alt="">
+        <div class="image">
+            <img src="<?= $image ?>" alt="">
+        </div>
+        <div class="vraag">
+            <p><?= $question ?></p>
+        </div>
+        <div class="opties">
+            <button id="optie">Optie 1</button>
+            <button id="optie">Optie 2</button>
+            <button id="optie">Optie 3</button>
+            <button id="optie">Optie 4</button>
+        </div>
     </div>
-    <div class="vraag">
-        <p><?=$question?></p>
-    </div>
-    <div class="opties">
-        <button id="optie">Optie 1</button>
-        <button id="optie">Optie 2</button>
-        <button id="optie">Optie 3</button>
-        <button id="optie">Optie 4</button>
-    </div>
-    </div>
-    
+
 </body>
 <footer>
     <div class="pagebuttons">
-        <button class="pagebutton">vorige</button>
+        <button class="pagebutton">Vorige</button>
         <button class="pagebutton">Volgende</button>
     </div>
 </footer>
+
 </html>
