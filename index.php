881ec1719e76082519564d6b179f6bd9f6ca4a15
TimPahlplatz <Tim.pahlplatz@gmail.com>
2024-05-29 11:19:20 +0200

    start

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/stylesheet.css">
+    <title>CBRtheory</title>
+</head>
+<body>
+    <div class="hoofdpagina">
+    <h1>Welkom bij deze CBR theory les</h1>
+    </div>
+    <div id="test">
+        <button class="knopje">Start</button>
+    </div>
+</body>
+</html>
