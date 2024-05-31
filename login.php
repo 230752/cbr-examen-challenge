fe41b4385ff64fd8215836ef58e3107ea3e1b054
TwanAsselbergs <twanasselbergs@outlook.com>
2024-05-31 09:32:56 +0200

    favicon

--- login.php
+++ login.php
@@ -20,6 +20,7 @@ if (isset($_POST['login'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/login.css">
+    <link rel="shortcut icon" href="https://www.cbr.nl/logo.jpg" type="image/x-icon" />
     <title>test login</title>
 </head>
 <script>
